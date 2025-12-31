f269d198226e1dba089c5307129e16a5bbc01514
human-12 <cafeofprogrammer@gmail.com>
2025-12-31 17:46:19 +0500

    done

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-
+import { base } from 'framer-motion/client';
+  
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
+    
+    plugins: [react()];
+    base:"/portfolio" ;
+    
     return {
       server: {
         port: 3000,
